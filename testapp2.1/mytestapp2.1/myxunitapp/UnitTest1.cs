2be8889801722a46df76265cc1c7f0459902d5f7
Dogancan Kasap <dgncn.ksp@gmail.com>
2020-12-20 15:06:39 +0300

    travis added

---
+++ testapp2.1/mytestapp2.1/myxunitapp/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace myxunitapp
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+
+        }
+    }
+}
